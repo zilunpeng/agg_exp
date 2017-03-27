f4df287d588b7e82a22564b5d98babcf750c55f0
Zilun Peng <zilunpeng@gmail.com>
2017-03-27 11:14:10 -0700

    change variable names

--- mln_direct_LR_wgtsForEachItem_oneHidden.py
+++ mln_direct_LR_wgtsForEachItem_oneHidden.py
@@ -137,7 +137,7 @@ for (user, item, rating, timestamp) in ratings:
     elif user not in user_neg_ratings_stats and rating < threshold:
         user_neg_ratings_stats[user] = [item]
 
-wgts_hidden = np.zeros(max(all_items))
+wgts_r_h = np.zeros(max(all_items))
 w0 = sigmoid_inv(original_ds.nf_tr/original_ds.tot_tr)
 wgt_h_g,wgt_nh_g = 1,-1
 iter = 0
@@ -148,13 +148,15 @@ def pred_mln(user, ds=original_ds, para=lambda x: 0):
     phgr = w0
     if user in user_pos_ratings_stats:
         for pos_item in user_pos_ratings_stats[user]:
-            phgr += wgts_hidden[pos_item-1]
+            phgr += wgts_r_h[pos_item-1]
+        for neg_item in user_pos_ratings_stats[user]:
+            phgr += wgts_r_h[neg_item-1]
     phgr = sigmoid(phgr)
     return sigmoid(wgt_h_g)*phgr+sigmoid(wgt_nh_g)*(1-phgr)
 
 
 def learn(num_iter=20, ds=original_ds, step_size=1e-5, pregl=0, trace=True):
-    global w0, iter, wgts_hidden, wgt_h_g, wgt_nh_g, phgr
+    global w0, iter, wgts_r_h, wgt_h_g, wgt_nh_g, phgr
     sse, sll = 0, 0
     prev_sll = float("inf")
     training_users = tuple(ds.gender_train)
@@ -172,10 +174,10 @@ def learn(num_iter=20, ds=original_ds, step_size=1e-5, pregl=0, trace=True):
                 w0 += step_size * sdiff * phgr * (1 - phgr) / predn
                 if user in user_pos_ratings_stats:
                     for pos_item in user_pos_ratings_stats[user]:
-                        wgts_hidden[pos_item-1] += step_size * sdiff * phgr * (1 - phgr) / predn
+                        wgts_r_h[pos_item-1] += step_size * sdiff * phgr * (1 - phgr) / predn
                 if user in user_neg_ratings_stats:
                     for neg_item in user_neg_ratings_stats[user]:
-                        wgts_hidden[neg_item - 1] += step_size * sdiff * phgr * (1 - phgr) / predn
+                        wgts_r_h[neg_item - 1] += step_size * sdiff * phgr * (1 - phgr) / predn
             else:
                 error = predn
                 sse += error ** 2
@@ -185,10 +187,10 @@ def learn(num_iter=20, ds=original_ds, step_size=1e-5, pregl=0, trace=True):
                 w0 -= step_size * sdiff * phgr * (1 - phgr) / (1 - predn)
                 if user in user_pos_ratings_stats:
                     for pos_item in user_pos_ratings_stats[user]:
-                        wgts_hidden[pos_item - 1] -= step_size * sdiff * phgr * (1 - phgr) / (1 - predn)
+                        wgts_r_h[pos_item - 1] -= step_size * sdiff * phgr * (1 - phgr) / (1 - predn)
                 if user in user_neg_ratings_stats:
                     for neg_item in user_neg_ratings_stats[user]:
-                        wgts_hidden[neg_item - 1] -= step_size * sdiff * phgr * (1 - phgr) / (1 - predn)
+                        wgts_r_h[neg_item - 1] -= step_size * sdiff * phgr * (1 - phgr) / (1 - predn)
         iter += 1
         if trace:
             print("iteration", iter, "wts for G(U)=", w0, "ase=", sse / len(ds.gender_train), "all=",
@@ -201,6 +203,6 @@ def learn(num_iter=20, ds=original_ds, step_size=1e-5, pregl=0, trace=True):
     print("after", iter, "iterations: evaluation=", ds.evaluate(pred_mln))
 
 
-learn(10000, trace=False)
+learn(6500, trace=False)
 # learn(10000, trace=False)
 # learn(3)
