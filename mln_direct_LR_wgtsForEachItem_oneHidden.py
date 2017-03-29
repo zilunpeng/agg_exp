9861df6ca47c52fb6f9d052327b175a252202b3d
Zilun Peng <zilunpeng@gmail.com>
2017-03-29 09:33:44 -0700

    no wgts for rpos-h

--- mln_direct_LR_wgtsForEachItem_oneHidden.py
+++ mln_direct_LR_wgtsForEachItem_oneHidden.py
@@ -15,7 +15,7 @@
 # In[1]:
 import math, random
 
-datasetname = "1m" # "100k"  # "1m"  # or "Yelp"
+datasetname = "Yelp" # "100k"  # "1m"  # or "Yelp"
 if datasetname=="100k":
     datafile, userfilename = "ml-100k/u.data","ml-100k/u.user"
     rating_cutoff, test_cutoff = 884673930, 880845177
@@ -178,7 +178,8 @@ for (user, item, rating, timestamp) in ratings:
     elif user not in user_neg_ratings_stats and rating < threshold:
         user_neg_ratings_stats[user] = [item]
 
-wgts_r_h = np.zeros(max(all_items))
+wgts_posr_h = np.zeros(max(all_items))
+wgts_negr_h = np.zeros(max(all_items))
 w0 = sigmoid_inv(original_ds.nf_tr/original_ds.tot_tr)
 wgt_h_g,wgt_nh_g = 1,-1
 iter = 0
@@ -189,15 +190,15 @@ def pred_mln(user, ds=original_ds, para=lambda x: 0):
     phgr = w0
     if user in user_pos_ratings_stats:
         for pos_item in user_pos_ratings_stats[user]:
-            phgr += wgts_r_h[pos_item-1]
+            phgr += wgts_posr_h[pos_item-1]
         for neg_item in user_pos_ratings_stats[user]:
-            phgr += wgts_r_h[neg_item-1]
+            phgr += wgts_negr_h[neg_item-1]
     phgr = sigmoid(phgr)
     return sigmoid(wgt_h_g)*phgr+sigmoid(wgt_nh_g)*(1-phgr)
 
 
 def learn(num_iter=20, ds=original_ds, step_size=1e-5, pregl=0, trace=True):
-    global w0, iter, wgts_r_h, wgt_h_g, wgt_nh_g, phgr
+    global w0, iter, wgts_posr_h, wgts_negr_h, wgt_h_g, wgt_nh_g, phgr
     sse, sll = 0, 0
     prev_sll = float("inf")
     training_users = tuple(ds.gender_train)
@@ -215,10 +216,10 @@ def learn(num_iter=20, ds=original_ds, step_size=1e-5, pregl=0, trace=True):
                 w0 += step_size * sdiff * phgr * (1 - phgr) / predn
                 if user in user_pos_ratings_stats:
                     for pos_item in user_pos_ratings_stats[user]:
-                        wgts_r_h[pos_item-1] += step_size * sdiff * phgr * (1 - phgr) / predn
+                        wgts_posr_h[pos_item-1] += step_size * sdiff * phgr * (1 - phgr) / predn
                 if user in user_neg_ratings_stats:
                     for neg_item in user_neg_ratings_stats[user]:
-                        wgts_r_h[neg_item - 1] += step_size * sdiff * phgr * (1 - phgr) / predn
+                        wgts_negr_h[neg_item - 1] += step_size * sdiff * phgr * (1 - phgr) / predn
             else:
                 error = predn
                 sse += error ** 2
@@ -228,20 +229,24 @@ def learn(num_iter=20, ds=original_ds, step_size=1e-5, pregl=0, trace=True):
                 w0 -= step_size * sdiff * phgr * (1 - phgr) / (1 - predn)
                 if user in user_pos_ratings_stats:
                     for pos_item in user_pos_ratings_stats[user]:
-                        wgts_r_h[pos_item - 1] -= step_size * sdiff * phgr * (1 - phgr) / (1 - predn)
+                        wgts_posr_h[pos_item - 1] -= step_size * sdiff * phgr * (1 - phgr) / (1 - predn)
                 if user in user_neg_ratings_stats:
                     for neg_item in user_neg_ratings_stats[user]:
-                        wgts_r_h[neg_item - 1] -= step_size * sdiff * phgr * (1 - phgr) / (1 - predn)
+                        wgts_negr_h[neg_item - 1] -= step_size * sdiff * phgr * (1 - phgr) / (1 - predn)
         iter += 1
-        if trace:
-            print("iteration", iter, "wts for G(U)=", w0, "ase=", sse / len(ds.gender_train), "all=",
-                  sll / len(ds.gender_train))
+        # if trace:
+            # print("iteration", iter, "wts for G(U)=", w0, "ase=", sse / len(ds.gender_train), "all=",
+            #       sll / len(ds.gender_train))
         if prev_sll < sll:
             # step_size *= 0.5
-            if trace: print("worsening step; step size", step_size)
+            if trace:
+                print("worsening step; step size", step_size)
+                print("worsening step; w0", w0)
+                print("worsening step; wgt_h_g", wgt_h_g)
+                print("worsening step; wgt_nh_g", wgt_nh_g)
             # w0,w1,w2 = old_weights
             prev_sll = sll
     print("after", iter, "iterations: evaluation=", ds.evaluate(pred_mln))
 
 
-learn(6500, trace=False)
+learn(5000, trace=True)
