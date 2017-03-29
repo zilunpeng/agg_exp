1ddc77c67778b513ce72d57131366e1df6128c83
Zilun Peng <zilunpeng@gmail.com>
2017-03-28 21:15:45 -0700

    multiple datasets

--- mln_direct_LR_wgtsForEachItem_oneHidden.py
+++ mln_direct_LR_wgtsForEachItem_oneHidden.py
@@ -15,32 +15,73 @@
 # In[1]:
 import math, random
 
-with open("ml-100k/u.data", 'r') as ratingsfile:
-    all_ratings = (tuple(int(e) for e in line.strip().split('\t'))
-                   for line in ratingsfile)
-    ratings = [eg for eg in all_ratings if eg[3] <= 884673930]
-    all_users = {u for (u, i, r, d) in ratings}
-    all_items = {i for (u, i, r, d) in ratings}
-    print("There are ", len(ratings), "ratings and", len(all_users), "users")
-    print("There are ", len(all_items), "items")
-    training_users = {u for (u, i, r, d) in ratings if d <= 880845177}
-    test_users = all_users - training_users
+datasetname = "1m" # "100k"  # "1m"  # or "Yelp"
+if datasetname=="100k":
+    datafile, userfilename = "ml-100k/u.data","ml-100k/u.user"
+    rating_cutoff, test_cutoff = 884673930, 880845177
+elif datasetname=="1m":
+    datafile, userfilename = "ml-1m/ratings.dat", "ml-1m/users.dat"
+    rating_cutoff, test_cutoff = 974687810, 967587781
+elif datasetname=="Yelp":
+    datafile, usertrainfilename, usertestfilename = "new MC/yelp_mc_reviews.csv", "new MC/yelp_mc_class_train.csv","new MC/yelp_mc_class_test.csv"
+else:
+    assert False, ("not a valid dataset name",datasetname)
 
-# extract the training and test dictionaries
-with open("ml-100k/u.user", 'r') as usersfile:
-    user_info = (line.strip().split('|') for line in usersfile)
-    gender_train, gender_test = {}, {}
-    for (u, a, g, o, p) in user_info:
-        if int(u) in training_users:
-            gender_train[int(u)] = g
-        elif int(u) in test_users:
-            gender_test[int(u)] = g
+def extract_cols(lst,indexes):
+    """extract sublist given by indexes from lst"""
+    return (lst[i] for i in indexes)
+
+with open(datafile,'r') as ratingsfile:
+    if datasetname == "Yelp":
+        ratings = [(int(user[1:]),int(rest[1:]),int(rating),99999) for line in ratingsfile
+                       for (user,rest,rating) in [tuple(line.strip().split(','))]   # for yelp
+                             ]
+        with open(usertrainfilename) as usertrainfile:
+            gender_train = {rest:"F" if eth=="Mexican" else "M" for line in usertrainfile
+                                for (rest,eth) in [tuple(line.strip().split(','))]
+                                }
+        with open(usertestfilename) as usertestfile:
+            gender_test = {rest:"F" if eth=="Mexican" else "M" for line in usertestfile
+                               for (rest,eth) in [tuple(line.strip().split(','))]
+                                }
+        training_users = set(gender_train)
+        test_users = set(gender_test)
+        all_users = training_users | test_users
+        all_items = {(rest) for (usr,rest,r,tmp) in ratings}
+        #assert all_users == {r for (r,u,i,d) in ratings}
+    else:
+        if datasetname == "100k":
+            all_ratings = (tuple(int(e) for e in line.strip().split('\t'))   # for 100k
+                             for line in ratingsfile)
+        elif datasetname == "1m":
+            all_ratings = (tuple(int(e) for e in extract_cols(line.strip().split(':'),[0,2,4,6])) # for 1m
+                         for line in ratingsfile)
+        ratings = [eg for eg in all_ratings if eg[3] <= rating_cutoff]
+        all_users = {u for (u,i,r,d) in ratings}
+        all_items = {i for (u, i, r, d) in ratings}
+        print("There are ",len(ratings),"ratings and",len(all_users),"users")
+        training_users = {u for (u,i,r,d) in ratings if d <= test_cutoff}
+        test_users = all_users - training_users
+
+        # extract the training and test dictionaries
+        with open(userfilename,'r') as usersfile:
+            if datasetname == "100k":
+                user_info = (line.strip().split('|') for line in usersfile)
+            elif datasetname == "1m":
+                user_info = (extract_cols(line.strip().split(':'),[0,4,2,6,8]) for line in usersfile)
+            gender_train, gender_test = {},{}
+            for (u,a,g,o,p) in user_info:
+                if int(u) in training_users:
+                    gender_train[int(u)]=g
+                elif int(u) in test_users:
+                    gender_test[int(u)]=g
 
 # check the results
 assert len(training_users) == len(gender_train)
 assert len(test_users) == len(gender_test)
 print("There are ", len(gender_train), "users for training")
 print("There are ", len(gender_test), "users for testing")
+print("There are ", len(all_items), "items")
 
 # In[2]:
 
@@ -204,5 +245,3 @@ def learn(num_iter=20, ds=original_ds, step_size=1e-5, pregl=0, trace=True):
 
 
 learn(6500, trace=False)
-# learn(10000, trace=False)
-# learn(3)
